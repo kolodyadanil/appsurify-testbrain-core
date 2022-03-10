e376e2c06e41942c2bad61aa5f4ecf9490e88870
Artem Demidenko <ar.demidenko@gmail.com>
2022-03-10 13:57:52 +0300

    Remote unused adapters

--- src/applications/allauth/socialaccount/providers/tumblr/urls.py
+++
@@ -1,6 +0,0 @@
-from applications.allauth.socialaccount.providers.oauth.urls import default_urlpatterns
-
-from .provider import TumblrProvider
-
-
-urlpatterns = default_urlpatterns(TumblrProvider)
