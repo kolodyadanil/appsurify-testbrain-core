db70a2480f270dcdde1fe336f2655a74b71ead51
Artem Demidenko <ar.demidenko@gmail.com>
2022-03-14 17:31:52 +0300

    Fix notification Split demons for notification and ML works

--- src/applications/ml/models.py
+++ src/applications/ml/models.py
@@ -1,71 +1,24 @@
 # -*- coding: utf-8 -*-
 import pathlib
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 from django.db import models
 from django.conf import settings
-
-
-class MLDataset(models.Model):
-
-    class Status(models.TextChoices):
-        PENDING = "PENDING", "PENDING"
-        STARTED = "STARTED", "STARTED"
-        SUCCESS = "SUCCESS", "SUCCESS"
-        REJECTED = "REJECTED", "REJECTED"
-        FAILURE = "FAILURE", "FAILURE"
-        UNKNOWN = "UNKNOWN", "UNKNOWN"
-
-    test_suite = models.OneToOneField(
-        "testing.TestSuite",
-        verbose_name="test_suite",
-        related_name="dataset",
-        on_delete=models.CASCADE
-    )
-
-    status = models.CharField(
-        verbose_name="status",
-        max_length=128,
-        default=Status.PENDING,
-        choices=Status.choices,
-        blank=False,
-        null=False
-    )
-
-    created = models.DateTimeField(
-        verbose_name="created",
-        auto_now_add=True,
-        help_text="Auto-generated field"
-    )
-
-    updated = models.DateTimeField(
-        verbose_name="updated",
-        auto_now=True,
-        help_text="Auto-generated and auto-updated field"
-    )
-
-    class Meta(object):
-        ordering = ["id", "test_suite", ]
-        verbose_name = "dataset"
-        verbose_name_plural = "datasets"
-
-    def __str__(self):
-        return f"{self.id}"
-
-    @property
-    def path(self):
-        project = self.test_suite.project
-        organization = project.organization
-        dir = pathlib.PosixPath(settings.STORAGE_ROOT) / "ml" / "datasets" / \
-              str(organization.id) / str(project.id)
-        return dir, f"{self.test_suite_id}.csv"
 
 
 class MLModel(models.Model):
 
     class Status(models.TextChoices):
-        PENDING = "PENDING", "PENDING"
-        STARTED = "STARTED", "STARTED"
-        SUCCESS = "SUCCESS", "SUCCESS"
-        FAILURE = "FAILURE", "FAILURE"
+        DATASET_PENDING = "DATASET_PENDING", "DATASET_PENDING"
+        DATASET_PROCESSING = "DATASET_PROCESSING", "DATASET_PROCESSING"
+        DATASET_SUCCESS = "DATASET_SUCCESS", "DATASET_SUCCESS"
+        DATASET_FAILURE = "DATASET_FAILURE", "DATASET_FAILURE"
+        DATASET_UNKNOWN = "DATASET_UNKNOWN", "DATASET_UNKNOWN"
+        MODEL_PENDING = "MODEL_PENDING", "MODEL_PENDING"
+        MODEL_PROCESSING = "MODEL_PROCESSING", "MODEL_PROCESSING"
+        MODEL_SUCCESS = "MODEL_SUCCESS", "MODEL_SUCCESS"
+        MODEL_FAILURE = "MODEL_FAILURE", "MODEL_FAILURE"
+        MODEL_UNKNOWN = "MODEL_UNKNOWN", "MODEL_UNKNOWN"
         UNKNOWN = "UNKNOWN", "UNKNOWN"
 
     test_suite = models.OneToOneField(
@@ -78,7 +31,7 @@ class MLModel(models.Model):
     status = models.CharField(
         verbose_name="status",
         max_length=128,
-        default=Status.PENDING,
+        default=Status.UNKNOWN,
         choices=Status.choices,
         blank=False,
         null=False
@@ -102,12 +55,44 @@ class MLModel(models.Model):
         verbose_name_plural = "models"
 
     def __str__(self):
-        return f"{self.id}"
+        return f"<Model: {self.id} (TestSuite: {self.test_suite_id})>"
 
     @property
-    def path(self):
+    def dataset_path(self):
+        project = self.test_suite.project
+        organization = project.organization
+        dir = pathlib.PosixPath(settings.STORAGE_ROOT) / "ml" / "datasets" / \
+              str(organization.id) / str(project.id)
+        return dir, f"{self.test_suite_id}.csv"
+
+    @property
+    def model_path(self):
         project = self.test_suite.project
         organization = project.organization
         dir = pathlib.PosixPath(settings.STORAGE_ROOT) / "ml" / "models" / \
               str(organization.id) / str(project.id)
         return dir, f"{self.test_suite_id}.model"
+
+
+@receiver(post_save, sender=MLModel)
+def create_directories(sender, instance, created, **kwargs):
+    try:
+        model_path, model_filename = instance.model_path
+        model_path.mkdir(parents=True, exist_ok=True)
+
+        dataset_path, dataset_filename = instance.dataset_path
+        dataset_path.mkdir(parents=True, exist_ok=True)
+    except Exception as e:
+        print(e)
+
+
+@receiver(post_delete, sender=MLModel)
+def delete_directories(sender, instance, **kwargs):
+    try:
+        model_path, model_filename = instance.model_path
+        model_path.rmdir()
+
+        dataset_path, dataset_filename = instance.dataset_path
+        dataset_path.rmdir()
+    except Exception as e:
+        print(e)
