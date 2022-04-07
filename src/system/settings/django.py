a1ce5edc9c2c7d7f8ab0f63a787e47b1ac875afc
Danil Kolodyazhnyi <kolodyadanil@gmail.com>
2022-04-07 14:14:27 +0200

    temporarily disable csrf

--- src/system/settings/django.py
+++ src/system/settings/django.py
@@ -95,7 +95,7 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     # "django.middleware.csrf.CsrfViewMiddleware",
-    "applications.disable_csrf_check.apps.DisableCSRF",
+    "applications.disable_csrf_check.apps.DisableCSRFMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
