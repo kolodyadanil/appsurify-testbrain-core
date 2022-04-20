0ff108734194edd6728bdd5abc14687da7f12b5f
Aleksandr Balashenko <a.balashenko@dinamicka.com>
2022-04-20 19:18:31 +0300

    header csp

--- src/system/settings/csp.py
+++ src/system/settings/csp.py
@@ -1,5 +1,23 @@
-CSP_DEFAULT_SRC = ("'none'",)
+# default source as self
+CSP_DEFAULT_SRC = ("'self'", )
+
+# style
 CSP_STYLE_SRC = ("'self'",)
+
+# scripts from our domain and other domains
 CSP_SCRIPT_SRC = ("'self'",)
-CSP_FONT_SRC = ("'self'",)
+
+# images from our domain and other domains
 CSP_IMG_SRC = ("'self'",)
+
+# loading manifest, workers, frames, etc
+CSP_FONT_SRC = ("'self'", )
+CSP_CONNECT_SRC = ("'self'",)
+CSP_OBJECT_SRC = ("'self'", )
+CSP_BASE_URI = ("'self'", )
+CSP_FRAME_ANCESTORS = ("'self'", )
+CSP_FORM_ACTION = ("'self'", )
+CSP_INCLUDE_NONCE_IN = ('script-src', )
+CSP_MANIFEST_SRC = ("'self'", )
+CSP_WORKER_SRC = ("'self'", )
+CSP_MEDIA_SRC = ("'self'", )
