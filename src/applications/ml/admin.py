e7cf7e9bb66b8176a78ee9fb7af52725f296e3ff
Artem Demidenko <ar.demidenko@gmail.com>
2022-04-21 00:35:16 +0300

    improve machine learning

--- src/applications/ml/admin.py
+++ src/applications/ml/admin.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.db import models
 from .models import MLModel
 
 
 class MLModelAdmin(admin.ModelAdmin):
-    list_display = ('id', 'test_suite', 'state', 'index', 'fr', 'to', 'created', 'updated')
+    list_display = ('id', 'test_suite', 'test_count', 'state', 'index', 'fr', 'to', 'created', 'updated')
+
+    def get_queryset(self, request):
+        qs = super(MLModelAdmin, self).get_queryset(request)
+        return qs.annotate(_test_count=models.Count('tests', distinct=True))
+
+    def test_count(self, obj):
+        return obj._test_count
+
+    test_count.short_description = 'tests'
+    test_count.admin_order_field = '_test_count'
 
 
 admin.site.register(MLModel, MLModelAdmin)
